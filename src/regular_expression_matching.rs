2730481a071f34f81e723b67c0f3dd5ad97f10ea
Amr Ayman <miroamr78@gmail.com>
2023-03-14 22:07:11 +0200

    examples pass!

--- src/regular_expression_matching.rs
+++ src/regular_expression_matching.rs
@@ -1,5 +1,5 @@
 pub fn is_match(s: String, p: String) -> bool {
-    is_there_a_match(&s, &s)
+    is_there_a_match(&s, &p)
 }
 
 fn is_there_a_match(s: &str, p: &String) -> bool {
@@ -11,22 +11,44 @@ fn is_there_a_match(s: &str, p: &String) -> bool {
     //    no? pass on the string right after that first character
 
     // (1)
-    let mut pi = p.chars();
-    if let Some(mut pc) = pi.next() {
-        let match_pc = |c| pc == '.' || c == pc;
-        if let Some(idx_s) = s.find(match_pc) {
-            // (2), (3)
-            return is_there_a_match_from_here(&s[idx_s..], p, false)
-                || is_there_a_match(&s[idx_s + 1..], p);
-        }
+    if p.len() == 0 {
+        return false;
     }
-    false
+    let pc = p.as_bytes()[0] as char;
+    let match_pc = |c| pc == '.' || c == pc;
+
+    if let Some(idx_s) = s.find(match_pc) {
+        // (2), (3)
+        return is_there_a_match_from_here(
+            &s[idx_s..],
+            p,
+            LoopVars {
+                repeated_once: false,
+                idx_s: 0,
+                idx_p: 0,
+            },
+        ) || is_there_a_match(&s[idx_s + 1..], p);
+    }
+    s.len() == 0 || can_pattern_be_ignored(p)
 }
 
 struct LoopVars {
-    did_the_first_plus: bool,
+    repeated_once: bool,
     idx_s: usize,
     idx_p: usize,
+}
+
+fn can_pattern_be_ignored(p: &str) -> bool {
+    if p.len() < 2 || p.len() % 2 == 1 {
+        return false;
+    }
+    for pc in p.chars().skip(1).step_by(2) {
+        println!("pc is {pc}");
+        if pc != '*' {
+            return false;
+        }
+    }
+    return true;
 }
 
 fn is_there_a_match_from_here(s: &str, p: &str, loop_vars: LoopVars) -> bool {
@@ -42,85 +64,99 @@ fn is_there_a_match_from_here(s: &str, p: &str, loop_vars: LoopVars) -> bool {
     // let mut po = pi.next();
     // let mut so = si.next();
 
-    if s.len() >= loop_vars.idx_s || p.len() >= loop_vars.idx_p {
+    if p.len() <= loop_vars.idx_p {
+        // pattern ended
         return true;
     }
 
-    let c = s[loop_vars.idx_s] as char;
-    let p = p[loop_vars.idx_p] as char;
+    if s.len() <= loop_vars.idx_s {
+        // string ended
+        // can the rest of the pattern be ignored?
+        return can_pattern_be_ignored(&p[loop_vars.idx_p..]);
+    }
+
+    let c = s.as_bytes()[loop_vars.idx_s] as char;
+    let pc = p.as_bytes()[loop_vars.idx_p] as char;
     let p_look_ahead = if p.len() > loop_vars.idx_p + 1 {
-        p[loop_vars.idx_p + 1] as char
+        p.as_bytes()[loop_vars.idx_p + 1] as char
     } else {
         '_'
     };
 
-    let match_em = || p == '.' || c == p;
+    let match_em = || pc == '.' || c == pc;
     let repeat = || {
+        println!("repeat");
         // string shifted, same pattern
         is_there_a_match_from_here(
             s,
             p,
             LoopVars {
-                did_the_first_plus: true,
+                repeated_once: true,
                 idx_s: loop_vars.idx_s + 1,
                 idx_p: loop_vars.idx_p,
             },
-        );
+        )
     };
 
     let consume = || {
         // string shifted, pattern shifted
+        println!("consume");
         is_there_a_match_from_here(
             s,
             p,
             LoopVars {
-                did_the_first_plus: false,
+                repeated_once: false,
                 idx_s: loop_vars.idx_s + 1,
                 idx_p: loop_vars.idx_p + 2,
             },
-        );
+        )
     };
 
     let consume_letter = || {
+        println!("consume_letter");
+
         // string shifted, pattern shifted
         is_there_a_match_from_here(
             s,
             p,
             LoopVars {
-                did_the_first_plus: false,
+                repeated_once: false,
                 idx_s: loop_vars.idx_s + 1,
                 idx_p: loop_vars.idx_p + 1,
             },
-        );
+        )
     };
 
     let ignore = || {
+        println!("ignore");
+
         // same string, pattern shifted
         is_there_a_match_from_here(
             s,
             p,
             LoopVars {
-                did_the_first_plus: false,
+                repeated_once: false,
                 idx_s: loop_vars.idx_s,
                 idx_p: loop_vars.idx_p + 2,
             },
-        );
+        )
     };
 
     // let repeat = || is_there_a_match_from_here(/* same string shifted right */, /* same pattern */, true);
     // let consume = || is_there_a_match_from_here(/* same string shifted right */, /* pattern after pd */);
 
-    if let ('*', _) = (p_look_ahead, did_the_first_plus) {
-        // just ignore it
-    } else if !match_em() {
+    if !match_em() {
+        if p_look_ahead == '*' || (p_look_ahead == '+' && loop_vars.repeated_once) {
+            return ignore();
+        }
         return false;
     }
 
     // adjust loop variables
-    match (p_look_ahead, did_the_first_plus) {
+    match (p_look_ahead, loop_vars.repeated_once) {
         ('+', false) => repeat(),
         ('+', true) => repeat() || consume(),
-        ('*', _) => repeat() || consume() || ignore(),
+        ('*', _) => repeat() || consume(),
         (_, _) => consume_letter(),
     }
 
--- tests/regular_expression_matching.rs
+++ tests/regular_expression_matching.rs
@@ -1,5 +1,29 @@
 #[cfg(test)]
 mod regular_expression_matching {
+    use solver::regular_expression_matching::is_match;
+
     #[test]
-    fn example1() {}
+    fn example1() {
+        assert_eq!(is_match("aa".into(), "aa".into()), true);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(is_match("aa".into(), "a*".into()), true);
+    }
+
+    #[test]
+    fn example3() {
+        assert_eq!(is_match("b".into(), "ab*".into()), false);
+    }
+
+    #[test]
+    fn example4() {
+        assert_eq!(is_match("b".into(), "b*b*ba*".into()), true);
+    }
+
+    #[test]
+    fn example5() {
+        assert_eq!(is_match("b".into(), "b*b+b*".into()), true);
+    }
 }
