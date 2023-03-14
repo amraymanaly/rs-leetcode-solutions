9eebbb00a50786699137b22bf1afc871b344bb3e
Amr Ayman <miroamr78@gmail.com>
2023-03-14 18:53:02 +0200

    Hint from a guy + Unsafe wizardry to speed up performance

--- src/longest_substring_without_repeating_characters.rs
+++ src/longest_substring_without_repeating_characters.rs
@@ -1,47 +1,22 @@
-// we want to find the length of the longest substring without repeating character LSWRC in a string
+use std::cmp::max;
 
 pub fn length_of_longest_substring(s: String) -> i32 {
-    // the idea is as follows:
-    // if i know (LSWRC overall, LSWRC including the last character) in a string
-    // then it's trivial to compute it again for that string concatenated with one character
-
-    // loop-invariant variables with 4 invariants
-    // best overall (1)
-    let mut overall = &"".to_string()[..];
-
-    // best edgy (2)
-    let mut edgy = overall.clone();
-
-    // &s[edgy_starts_from..] == edgy (3)
-    let mut edgy_starts_from = 0usize;
-
-    // for all 0 <= i < 27 --> edgy_matrix[i] == edgy.find(the corresponding char) (4)
-    let mut edgy_matrix: [Option<usize>; 178] = [None; 178];
-
-    // helper variables
+    let mut idx_s = 0usize;
+    let mut matrix: [Option<usize>; 178] = [None; 178];
+    let mut answer = 0;
     let mut temp;
 
-    for (mark, i) in s.as_bytes().iter().enumerate() {
-        if let Some(mut pos_in_edgy) = edgy_matrix[*i as usize] {
-            pos_in_edgy += 1;
-            edgy_starts_from += pos_in_edgy;
-
-            for c in edgy[..pos_in_edgy].as_bytes() {
-                edgy_matrix[*c as usize] = None;
-            }
-            for c in edgy[pos_in_edgy..].as_bytes() {
-                temp = edgy_matrix[*c as usize].unwrap();
-                edgy_matrix[*c as usize] = Some(temp - pos_in_edgy);
-            }
+    for (idx, b) in s.as_bytes().iter().enumerate() {
+        unsafe {
+            temp = matrix.get_unchecked_mut(*b as usize);
+        }
+        if let Some(collision) = *temp {
+            idx_s = max(collision + 1, idx_s);
         }
 
-        edgy = &s[edgy_starts_from..=mark];
-        edgy_matrix[*i as usize] = Some(mark - edgy_starts_from);
-
-        if edgy.len() > overall.len() {
-            overall = edgy;
-        }
+        answer = max(answer, idx - idx_s + 1);
+        *temp = Some(idx);
     }
 
-    overall.len() as i32
+    answer as i32
 }
