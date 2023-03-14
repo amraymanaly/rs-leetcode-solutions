b74c9811832eccd0a04cdd8a9431adcea0529484
Amr Ayman <miroamr78@gmail.com>
2023-03-14 20:55:41 +0200

    refined, before build

--- src/regular_expression_matching.rs
+++ src/regular_expression_matching.rs
@@ -16,18 +16,23 @@ fn is_there_a_match(s: &str, p: &String) -> bool {
         let match_pc = |c| pc == '.' || c == pc;
         if let Some(idx_s) = s.find(match_pc) {
             // (2), (3)
-            return is_there_a_match_from_here(&s[idx_s..], p)
+            return is_there_a_match_from_here(&s[idx_s..], p, false)
                 || is_there_a_match(&s[idx_s + 1..], p);
         }
     }
     false
 }
 
-fn is_there_a_match_from_here(s: &str, p: &str) -> bool {
+struct LoopVars {
+    did_the_first_plus: bool,
+    idx_s: usize,
+    idx_p: usize,
+}
+
+fn is_there_a_match_from_here(s: &str, p: &str, loop_vars: LoopVars) -> bool {
     // a version that sees if p matches s from the start, i.e, does not s.find
     // also, this version assumes that:
-    //      1. s and p are both non-empty
-    //      2. p is a valid pattern, i.e, doesn't start with + or *
+    //      1. p is a valid pattern, i.e, doesn't start with + or *
 
     // let match_pc = |c: char| pc == '.' || c == pc;
 
@@ -37,23 +42,87 @@ fn is_there_a_match_from_here(s: &str, p: &str) -> bool {
     // let mut po = pi.next();
     // let mut so = si.next();
 
-    let mut look_ahead = '_';
-
-    if look_ahead == '+' || look_ahead == '*' {
-        if is_there_a_match_from_here(/* same string */, /* same pattern */) ||
-           is_there_a_match_from_here(/* same string shifted right */, /* pattern after pd */) {
+    if s.len() >= loop_vars.idx_s || p.len() >= loop_vars.idx_p {
         return true;
-           }
     }
 
-    pc = look_ahead;
-    look_ahead = pi.next().unwrap_or('_'); // :D
-    so = si.next();
-    if !match_pc(sc) {
-        if look_ahead == '*' && so.is_some() {
-        return is_there_a_match_from_here(/* same string shifting right */, /* pattern after pd */);
-        }
+    let c = s[loop_vars.idx_s] as char;
+    let p = p[loop_vars.idx_p] as char;
+    let p_look_ahead = if p.len() > loop_vars.idx_p + 1 {
+        p[loop_vars.idx_p + 1] as char
+    } else {
+        '_'
+    };
+
+    let match_em = || p == '.' || c == p;
+    let repeat = || {
+        // string shifted, same pattern
+        is_there_a_match_from_here(
+            s,
+            p,
+            LoopVars {
+                did_the_first_plus: true,
+                idx_s: loop_vars.idx_s + 1,
+                idx_p: loop_vars.idx_p,
+            },
+        );
+    };
+
+    let consume = || {
+        // string shifted, pattern shifted
+        is_there_a_match_from_here(
+            s,
+            p,
+            LoopVars {
+                did_the_first_plus: false,
+                idx_s: loop_vars.idx_s + 1,
+                idx_p: loop_vars.idx_p + 2,
+            },
+        );
+    };
+
+    let consume_letter = || {
+        // string shifted, pattern shifted
+        is_there_a_match_from_here(
+            s,
+            p,
+            LoopVars {
+                did_the_first_plus: false,
+                idx_s: loop_vars.idx_s + 1,
+                idx_p: loop_vars.idx_p + 1,
+            },
+        );
+    };
+
+    let ignore = || {
+        // same string, pattern shifted
+        is_there_a_match_from_here(
+            s,
+            p,
+            LoopVars {
+                did_the_first_plus: false,
+                idx_s: loop_vars.idx_s,
+                idx_p: loop_vars.idx_p + 2,
+            },
+        );
+    };
+
+    // let repeat = || is_there_a_match_from_here(/* same string shifted right */, /* same pattern */, true);
+    // let consume = || is_there_a_match_from_here(/* same string shifted right */, /* pattern after pd */);
+
+    if let ('*', _) = (p_look_ahead, did_the_first_plus) {
+        // just ignore it
+    } else if !match_em() {
         return false;
     }
-    true
+
+    // adjust loop variables
+    match (p_look_ahead, did_the_first_plus) {
+        ('+', false) => repeat(),
+        ('+', true) => repeat() || consume(),
+        ('*', _) => repeat() || consume() || ignore(),
+        (_, _) => consume_letter(),
+    }
+
+    // is_there_a_match_from_here(&s[1..], p, did_the_first_plus)
 }
